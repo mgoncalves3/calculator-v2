ba3ac4e94685084efe6a941e6b9b7e253fa105c1
mgoncalves3 <marco.g@sapo.pt>
2022-03-25 17:11:24 +0000

    Adds once

--- script.js
+++ script.js
@@ -1,14 +1,95 @@
-const numberButtons = document.getElementsByClassName("number");
+let updateDisplay = (e) => {
+  let currentDisplay;
+  if (defaultState) {
+    currentDisplay = ``;
+  } else {
+    currentDisplay = `${display.textContent}`;
+  }
+  defaultState = false;
+  let nextDisplay = `${currentDisplay}${e.target.value}`;
+  display.textContent = nextDisplay;
+};
+
+let defaultState = true;
 
 const display = document.getElementById("display");
-console.log(display);
-
-let updateVisor = (e) => {
-  let currentDisplay = `${display.textContent}`;
-  let nextDisplay = `${currentDisplay}${e.target.value}`
-  display.textContent = nextDisplay;
-}
+const clearButton = document.getElementById("clear");
+const numberButtons = document.getElementsByClassName("number");
+const addButton = document.getElementById("add");
+const equalsButton = document.getElementById("equals");
 
 for (const button of numberButtons) {
-  button.addEventListener("click", updateVisor);
+  button.addEventListener("click", updateDisplay);
 }
+
+const reset = () => {
+  display.textContent = "0";
+  num1 = "", num2 = "", operation = "", result = "";
+  defaultState = true;
+};
+
+clearButton.addEventListener("click", reset);
+
+let num1 = "";
+let operation = "";
+let num2 = "";
+let result = "";
+
+// Operations
+const add = () => {
+  num1 = parseInt(getCurrentDisplay());
+  operation = "+";
+  defaultState = true;
+};
+
+const subtract = () => {
+  num1 = parseInt(getCurrentDisplay());
+  operation = "-";
+  defaultState = true;
+};
+
+const multiply = () => {
+  num1 = parseInt(getCurrentDisplay());
+  operation = "*";
+  defaultState = true;
+};
+
+const divide = () => {
+  num1 = parseInt(getCurrentDisplay());
+  operation = "/";
+  defaultState = true;
+};
+
+const getCurrentDisplay = () => {
+  return display.textContent;
+};
+
+const updateFinalResult = (val = result) => {
+  display.textContent = `${val}`;
+  defaultState = true;
+};
+
+const evaluate = () => {
+  num2 = parseInt(getCurrentDisplay());
+  switch (operation) {
+    case "+":
+      result = num1 + num2;
+      updateFinalResult();
+      break;
+    case "-":
+      result = num1 - num2;
+      updateFinalResult();
+      break;
+    case "*":
+      result = num1 * num2;
+      updateFinalResult();
+      break;
+    case "/":
+      result = num1 / num2;
+      updateFinalResult();
+      break;
+  }
+};
+
+addButton.addEventListener("click", add);
+equalsButton.addEventListener("click", evaluate);
